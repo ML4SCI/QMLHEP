0656d5e4889fff1bdfa78df192d6cb3e6d69892f
Amey-2002 <asbhatuse_b20@ce.vjti.ac.in>
2022-09-27 00:44:59 +0530

    QGANS for HEP GSoC 2022

---
+++ Quantum_GAN_for_HEP_Amey_Bhatuse/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='QGANSHEP',
+    version='1.0.0',
+    author='Amey Bhatuse',
+    author_email='ameybhatuse315@gmail.com',
+    description="Quantum Generative Adversarial Neural Networks",
+    long_description="This package is a implementation of different models of quantum generative adversarial networks and few other utilities like a quantum convolutional layer for building custom models from scratch. The model can be trained using Keras API.",
+    packages=['QGANSHEP'],
+    url='https://github.com/Amey-2002/GSoC_2022_QMLHEP'
+)
