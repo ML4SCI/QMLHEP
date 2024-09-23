5f323fd7f45a6e412e09ad458f2302a27808ec3f
Haemanth V <haemanth10@gmail.com>
2024-09-23 01:33:10 -0700

    Update Custom_GATConv file

--- Quantum_GNN_for_HEP_Haemanth_Velmurugan/code/models/GATConvLayers/Custom_GATConv.py
+++ Quantum_GNN_for_HEP_Haemanth_Velmurugan/code/models/GATConvLayers/Custom_GATConv.py
@@ -1,44 +1,53 @@
 import torch
-from torch.nn import Linear, Parameter
+from torch.nn import Linear, Parameter, Sequential, ReLU, LeakyReLU
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, remove_self_loops
-from QNN_Node_Embedding import quantum_net
-from Quantum_Classifiers import MPS, TTN
 
 
-class QGATConv(MessagePassing):
-    def __init__(self, in_channels, depth, attn_model):
+class GATConv(MessagePassing):
+    def __init__(self, in_channels, out_channels):
         super().__init__(aggr='add')  # "Add" aggregation (Step 5).
-        self.bias = nn.Parameter(torch.empty(in_channels))
+#         self.heads = heads
+#         seld.concat = concat
+        self.lin = Linear(in_channels, out_channels, bias=False)
+        self.attn = Sequential(Linear(out_channels*2, 8),
+                               ReLU(),
+                               Linear(8, 1),
+                               LeakyReLU(0.2)
+                               )
+        total_out_channels = out_channels
+        self.bias = Parameter(torch.empty(total_out_channels))
         self.reset_parameters()
-        self.n_qubits = in_channels
-        self.qc, _ = quantum_net(self.n_qubits, depth)
-
-        if attn_model == "MPS":
-            self.attn, _ = MPS(in_channels*2)
-        else:
-            self.attn, _ = TTN(in_channels*2)
-
-        self.readout = Linear(1, 1)
-        self.attn = Linear(in_channels*2, 1)
 
     def reset_parameters(self):
+        super().reset_parameters()
+        self.lin.reset_parameters()
+
+        for layer in self.attn:
+            if hasattr(layer, 'reset_parameters'):
+                layer.reset_parameters()
+
         self.bias.data.zero_()
 
     def forward(self, x, edge_index, edge_attr):
         # x has shape [N, in_channels]
         # edge_index has shape [2, E]
 
+        # H, C = self.heads, self.out_channels
+
+        x = self.lin(x)  # .view(-1, H, C)
+
         # Add self-loops to the adjacency matrix.
         edge_index, edge_attr = remove_self_loops(edge_index, edge_attr)
         edge_index, edge_attr = add_self_loops(
             edge_index, edge_attr, num_nodes=x.size(0))
 
-        # Linearly transform node feature matrix
-        q_out = self.qc(x).float()
+        # alpha = self.edge_update(x_i, x_j, edge_attr=edge_attr)
 
-        # Start propagating messages.
-        out = self.propagate(edge_index, x=q_out)
+        # propagate_type: (x: OptPairTensor, alpha: Tensor)
+        out = self.propagate(edge_index, x=x)
+
+        # out = out.mean(dim=1)
 
         # Apply a final bias vector.
         out = out + self.bias
@@ -46,8 +55,6 @@ class QGATConv(MessagePassing):
         return out
 
     def message(self, x_i, x_j):
-
         x_edge = torch.cat((x_i, x_j), dim=1)
         x_edge = self.attn(x_edge)
-        x_edge = self.readout(x_edge)
         return x_edge.view(-1, 1) * x_j
