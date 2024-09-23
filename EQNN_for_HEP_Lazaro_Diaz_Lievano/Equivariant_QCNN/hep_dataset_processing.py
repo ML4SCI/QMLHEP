3da48331dde2e8cc2d2a5aee3a165ee30422eb95
Lazaro Raul Diaz Lievano <lazaro.diazl@alumno.buap.mx>
2024-09-23 05:57:28 -0600

    Final Submission GSoC

--- EQNN_for_HEP_Lazaro_Diaz_Lievano/Equivariant_QCNN/hep_dataset_processing.py
+++ EQNN_for_HEP_Lazaro_Diaz_Lievano/Equivariant_QCNN/hep_dataset_processing.py
@@ -6,17 +6,14 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt 
 
 
-# Cargar el archivo .h5
 def quark_gluon():  
-    file_path = "Equivariant_QCNN/hep_data/QG_16x16x1_dataset_50k"
+    file_path = "/home/lazaror/quantum/pruebas/Takhur_QCNN/Equivariant_QCNN/hep_data/QG_16x16x1_dataset_50k"
     with h5py.File(file_path, "r") as file:
         X = np.array(file["X"])
         y = np.array(file["y"])
 
-    # Dividir los datos en conjuntos de entrenamiento y prueba
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Convertir los datos a tensores de TensorFlow
     X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
     X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
     Y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
@@ -31,20 +28,21 @@ def quark_gluon():
 
 
 def electron_photon():
-    file_path = "Equivariant_QCNN/hep_data/electron.h5"
-    with h5py.File(file_path, "r") as file:
+    file_path_electron = "/home/lazaror/quantum/pruebas/Takhur_QCNN/Equivariant_QCNN/hep_data/electron.hdf5"
+    with h5py.File(file_path_electron, "r") as file:
         X_e = np.array(file["X"])
         y_e = np.array(file["y"])
 
-    file_path = "Equivariant_QCNN/hep_data/photon.h5"
-    with h5py.File(file_path, "r") as file:
+    file_path_photon = "/home/lazaror/quantum/pruebas/Takhur_QCNN/Equivariant_QCNN/hep_data/photon.hdf5"
+    with h5py.File(file_path_photon, "r") as file:
         X_p = np.array(file["X"])
         y_p = np.array(file["y"])
 
-    # Dividir los datos en conjuntos de entrenamiento y prueba
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X = np.concatenate((X_e, X_p), axis=0)
+    y = np.concatenate((y_e, y_p), axis=0)
 
-    # Convertir los datos a tensores de TensorFlow
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
     X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
     X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
     Y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
